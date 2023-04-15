23fece4907e07c5c2a736e1d33645d3ad0f47a97
rvvil <rvvil@DESKTOP-POIK77V>
2023-04-15 11:34:48 -0600

    Agregar archivos de proyecto.

---
+++ practica 15-04-2023/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace practica_15_04_2023.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
